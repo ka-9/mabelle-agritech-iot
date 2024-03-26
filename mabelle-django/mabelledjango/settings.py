ff39ed621a63e66e4ed56e23900965483b658adf
Kevin Aoun <kevin.w.aoun@gmail.com>
2024-03-26 15:14:23 +0200

    finalized setup of project

--- mabelle-django/mabelledjango/settings.py
+++ mabelle-django/mabelledjango/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "mabelleweb.urls"
+ROOT_URLCONF = "mabelledjango.urls"
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "mabelleweb.wsgi.application"
+WSGI_APPLICATION = "mabelledjango.wsgi.application"
 
 
 # Database
