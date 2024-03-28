670ba82365d3981c6cc3de78df9d67c7df84c317
Kevin Aoun <kevin.w.aoun@gmail.com>
2024-03-28 18:50:16 +0200

    created NPK data model

--- mabelle-django/core/models.py
+++ mabelle-django/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class NPKReading(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    nitrogen_level = models.DecimalField(max_digits=10, decimal_places=2)
+    phosphorus_level = models.DecimalField(max_digits=10, decimal_places=2)
+    potassium_level = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"NPK Sensor Reading - Timestamp: {self.timestamp}, Nitrogen Level: {self.nitrogen_level}, Phosphorus Level: {self.phosphorus_level}, Potassium Level: {self.potassium_level}"
+
