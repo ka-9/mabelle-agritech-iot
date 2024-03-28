67ff80b5ec8e4398e9007b2108091c55d3d509fa
Kevin Aoun <kevin.w.aoun@gmail.com>
2024-03-28 18:50:32 +0200

    created view to create NPK object from cloud data

--- mabelle-django/core/views.py
+++ mabelle-django/core/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import NPKReading
+from api.iot_cloud_fetch import get_access_token, fetch_data
 
-# Create your views here.
+access_token = get_access_token()
+
+def create_npk_objects(request):
+    data = fetch_data(access_token)
+
+    npk_values = []
+
+    if data:
+        for obj in data:
+            value_updated_at = obj.value_updated_at
+            npk_values.append(obj.last_value)
+            
+        # Create NPK object with the extracted attributes
+        npk_obj = NPKReading.objects.create(
+            timestamp = value_updated_at,
+            nitrogen_level = npk_values[0],
+            phosphorus_level = npk_values[1],
+            potassium_level = npk_values[2]
+        )
+
+        return JsonResponse({'message': 'NPK objects created successfully'}, status=200)
+    else:
+        return JsonResponse({'message': 'No data available'}, status=404)
